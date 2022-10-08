f13fa697ed7cfd0b3a2ad99609034b52ec5f55b6
huang06 <tom4996131207@gmail.com>
2022-10-08 09:30:19 +0800

    feat: update

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,11 +16,12 @@ def test_my_model_save():
     print(body)
 
 
-class TestMarkers:
-
-    def test_not_e2e(self):
-        print("this is not e2e test")
-
-    @pytest.mark.e2e
-    def test_e2e(self):
-        print("This is e2e test")
+@pytest.mark.e2e
+@mock_s3
+def test_my_model_save():
+    conn = boto3.resource('s3', region_name='us-east-1')
+    # We need to create the bucket since this is all in Moto's 'virtual' AWS account
+    conn.create_bucket(Bucket='mybucket')
+    model_instance = MyModel('steve', 'is awesome')
+    model_instance.save()
+    model_instance.download()
